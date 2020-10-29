cdfbd3ef190ecb6899e363defc6c4eceb01734ab
FmarcosDev <fmarcosdev@Gmail.com>
2020-10-29 00:31:26 -0300

    x
    x

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,9 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from "react"
 
-// You can delete this file if you're not using it
+//traigo el proveedor
+import { CartProvider } from "./src/context/CartContext"
+
+// regreso esto armado  con el PROVEEDOR
+export const wrapRootElement = ({ element }) => (
+  <CartProvider>{element}</CartProvider>
+)
